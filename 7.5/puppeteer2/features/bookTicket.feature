03f34224780bca88e94e3980b43a9d6509430e32
Olga Em <olga.em020@gmail.com>
2024-04-16 23:56:10 +0700

    Add new test cucumber

--- 7.5/puppeteer2/features/bookTicket.feature
+++ 7.5/puppeteer2/features/bookTicket.feature
@@ -1,9 +1,24 @@
 Feature: Ticket booking
 
-    Scenario: Should book ticket
+    Scenario: Book ticket VIP
         Given user is on page "https://qamid.tmweb.ru/client/index.php"
         When user selects the desired day
         Then user selects the desired movie
         Then user chooses a location
         Then user is booking tickets
         Then user confirms the booking "Забронировать"
+
+    Scenario: Book two tickets
+        Given user is on page "https://qamid.tmweb.ru/client/index.php"
+        When user selects the desired day2
+        Then user selects the desired movie2
+        Then user chooses a location2
+        Then user chooses a location3
+        Then user is booking tickets
+        Then user confirms the booking "Забронировать"
+
+    Scenario: Ticket already booked
+        Given user is on page "https://qamid.tmweb.ru/client/index.php"
+        When user selects the desired movie3
+        Then user chooses a book location
+        Then make sure that the Забронировать button is inactive
--- 7.5/puppeteer2/features/step_definitions/bookTicket.step.js
+++ 7.5/puppeteer2/features/step_definitions/bookTicket.step.js
@@ -46,3 +46,42 @@ Then("user confirms the booking {string}", async function (string) {
   const actual =await getText(this.page, ".acceptin-button");
   await expect(actual).contain("Забронировать");
 }, 70000);
+
+
+When("user selects the desired day2", async function () {
+  return await clickElement(this.page, "body > nav > a:nth-child(3)");
+}, 70000);
+
+Then ("user selects the desired movie2", async function () {
+  return await clickElement (
+    this.page,"body > main > section:nth-child(4) > div.movie-seances__hall > ul > li > a");
+}, 70000);
+
+Then ("user chooses a location2", async function () {
+  return await clickElement(this.page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(6) > span:nth-child(8)");
+}, 70000);
+
+Then ("user chooses a location3", async function () {
+  return await clickElement(this.page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(6) > span:nth-child(9)");
+}, 70000);
+
+When ("user selects the desired movie3", async function () {
+  return await clickElement (
+    this.page,"body > main > section:nth-child(2) > div.movie-seances__hall > ul > li:nth-child(2) > a");
+}, 70000);
+
+Then ("user chooses a book location", async function () {
+  return await clickElement(this.page, "body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(1) > span.buying-scheme__chair.buying-scheme__chair_standart.buying-scheme__chair_taken");
+}, 70000);
+
+Then ("make sure that the Забронировать button is inactive", async function () {
+  const actual = await this.page.$eval('.acceptin-button', (button) => {
+    return button.disabled;
+  });
+  expect(actual).toBe(true);
+}, 70000);
+
+
+
+
+
